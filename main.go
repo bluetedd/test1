406b4c1fb0aa1d8a2eb47d517cbe350ed589ebb0
ed <edward@clanrobinson.co.uk>
2020-01-22 21:18:07 +0000

    wible

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+/*
+func main() {
+	var i = 0
+	for i = 1; i != 100; {
+
+	rand.Seed(time.Now().UnixNano())
+
+	min := (i)
+	max := 101
+
+
+
+	var x = rand.Intn(max-min) + min
+
+	i = x
+
+	if (x > 50) {
+		fmt.Printf("%d is greater than 50\n", x)
+
+	} else {
+		fmt.Printf("%d is not greater than 50\n", x)
+	}
+
+
+	}
+
+
+
+
+}
